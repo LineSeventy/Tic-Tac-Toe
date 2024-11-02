91a72a54c7a889f37b96bae67c890538e9cd2485
LineSeventy <169619934+LineSeventy@users.noreply.github.com>
2024-11-02 21:26:17 +0800

    Ok state

--- Script/script.js
+++ Script/script.js
@@ -1,72 +1,77 @@
-let player1 = new playerSystem("X")
-let player2 = new playerSystem("O")
-
-
-
-function playerSystem (playerName,playerScore = 0,rounds = 0,wins = 0,turn = 0) {
-    this.playerName = playerName;
-    this.playerScore = playerScore;
-    this.rounds = rounds;
-    this.wins = wins;
-    this.turn = turn;
-}
-
-
-let playerTurnSystem = () =>{
-    if(player1.turn > player2.turn){
-        player2.turn += 1;
-        }else{
-            player1.turn += 1;
-        }
-        console.log(player1.turn +" " + player2.turn)
+class PlayerSystem {
+    constructor(playerName, playerScore = 0, rounds = 0, wins = 0) {
+        this.playerName = playerName;
+        this.playerScore = playerScore;
+        this.rounds = rounds;
+        this.wins = wins;
+        this.turn = 0; 
     }
 
+    turnPlus() {
+        this.turn += 1;
+    }
 
+    checkWin(gameBoard) {
+        const winReq = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
 
- 
-    let gameDisplay = () => {
-        let gameBoard = ["#","#","#","#","#","#","#","#","#"];
-        
-        for(let i = 0; i < 9; i++){
-            let replacePosition = prompt("Position 1-9") - 1;
-            let newValue = prompt("X or O");
-            gameBoard[replacePosition] = newValue;
-            console.log(gameBoard);
-            
-            if (checkWin(gameBoard)) {
-                console.log("We have a winner!");
-                return; 
+        for (const req of winReq) {
+            if (req.every(index => gameBoard[index] === this.playerName)) {
+                return true;
             }
-            
-            playerTurnSystem();  
         }
-        
-        function checkWin(gameBoard,player1,player2) {
-            const winReq = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ];
-            
-            for(const req of winReq){
-                if(req.every(index => gameBoard[index] === 'X')){
-                    return true;
-                    console.log(player1.playerName)
-                }
-                if(req.every(index => gameBoard[index] === 'O')){
-                    return true;
-                    console.log(player2.playerName)
+        return false;
+    }
+}
+
+let player1 = new PlayerSystem("X");
+let player2 = new PlayerSystem("O");
+
+let gameStart = () => {
+    let gameBoard = ["#", "#", "#", "#", "#", "#", "#", "#", "#"];
+    let currentPlayer = player1; 
+
+    const buttons = document.querySelectorAll('.buttonContainer button');
+
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            if (gameBoard[index] === '#') {
+                gameBoard[index] = currentPlayer.playerName; 
+                button.textContent = currentPlayer.playerName; 
+                if (currentPlayer.checkWin(gameBoard)) { 
+                    alert(`${currentPlayer.playerName} wins!`);
+                    resetGame(); 
+                } else {
+                    playerTurnSystem(); 
+                    currentPlayer = currentPlayer === player1 ? player2 : player1; 
                 }
             }
-            return false;
-        }
-    };
-    
+        });
+    });
 
 
-gameDisplay();
+    const playerTurnSystem = () => {
+        currentPlayer.turnPlus(); 
+    }
+
+    // Function to reset the game
+    function resetGame() {
+        gameBoard = ["#", "#", "#", "#", "#", "#", "#", "#", "#"];
+        buttons.forEach(button => {
+            button.textContent = '#'; 
+        });
+        player1.turn = 0; 
+        player2.turn = 0;
+        currentPlayer = player1; 
+    }
+}
+
+gameStart();
