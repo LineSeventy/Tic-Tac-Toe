40f654cfe99c48f1a9c1bd61e19fed0e93b377fa
LineSeventy <169619934+LineSeventy@users.noreply.github.com>
2024-11-02 17:59:51 +0800

    Its working atleast

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,8 @@
+let player1 = new playerSystem("X")
+let player2 = new playerSystem("O")
+
+
+
 function playerSystem (playerName,playerScore = 0,rounds = 0,wins = 0,turn = 0) {
     this.playerName = playerName;
     this.playerScore = playerScore;
@@ -6,9 +11,6 @@ function playerSystem (playerName,playerScore = 0,rounds = 0,wins = 0,turn = 0) 
     this.turn = turn;
 }
 
-let player1 = new playerSystem(prompt("Your Name"))
-let player2 = new playerSystem(prompt("Your Name"))
-console.log(player1.playerName)
 
 let playerTurnSystem = () =>{
     if(player1.turn > player2.turn){
@@ -20,19 +22,51 @@ let playerTurnSystem = () =>{
     }
 
 
-// let gameBoard = () => {
-    let board = [1,2,3,4,5,6,7,8,9];
 
-    for(let i = 0; i < 2; i++){
-    let replacePosition = prompt("Position") -1;
-    let newValue = prompt("X or O");
-    board[replacePosition] = newValue;
-    console.log(board);
-    playerTurnSystem();
+ 
+    let gameDisplay = () => {
+        let gameBoard = ["#","#","#","#","#","#","#","#","#"];
+        
+        for(let i = 0; i < 9; i++){
+            let replacePosition = prompt("Position 1-9") - 1;
+            let newValue = prompt("X or O");
+            gameBoard[replacePosition] = newValue;
+            console.log(gameBoard);
+            
+            if (checkWin(gameBoard)) {
+                console.log("We have a winner!");
+                return; 
+            }
+            
+            playerTurnSystem();  
+        }
+        
+        function checkWin(gameBoard,player1,player2) {
+            const winReq = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+            
+            for(const req of winReq){
+                if(req.every(index => gameBoard[index] === 'X')){
+                    return true;
+                    console.log(player1.playerName)
+                }
+                if(req.every(index => gameBoard[index] === 'O')){
+                    return true;
+                    console.log(player2.playerName)
+                }
+            }
+            return false;
+        }
+    };
+    
 
-    }
-// }
 
-
-
-// gameBoard();
+gameDisplay();
